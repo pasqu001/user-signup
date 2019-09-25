06d7d9a6fdc684327559ee25359874b6ce7d0aa3
pasqu001 <kaseypasqualini@yahoo.com>
2019-09-24 20:00:23 -0400

    fixed url issue with welcome page

--- main.py
+++ main.py
@@ -6,55 +6,57 @@ app = Flask(__name__)
 app.config['DEBUG'] = True 
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
 
 
-@app.route("/welcome", methods=['POST'])
+    if request.method=='POST':
+        name = request.form['username']
+        password = request.form['password']
+        password2 = request.form['password2']
+        email = request.form['email']
+
+        pass_error = ''
+        mail_error = ''
+        user_error = ''
+
+        if name == '':
+            user_error = user_error + 'Username cannot be blank. '
+        else:
+            if name.strip(' ') != name:
+                user_error = user_error + 'Spaces are not allowed in password. '
+            if len(name) < 3 or len(name) > 20:
+                user_error = user_error + 'Username has to be between 3 and 20 characters. '
+
+
+        if password == '':
+            pass_error = pass_error + 'Password Cannot be blank. '
+        else:
+            if password != password2:
+                pass_error = pass_error + 'Password does not Match. '
+            if password.strip(' ') != password:
+                pass_error = pass_error + 'Spaces are not allowed in password. '
+            if len(password) < 3 or len(password) > 20:
+                pass_error = pass_error + 'Password has to be between 3 and 20 characters. '
+
+        if email != '':
+            if email.strip(' ') != email:
+                mail_error = mail_error + 'You cannot have a space. '
+            if '@' not in email or '.' not in email:
+                mail_error = mail_error + 'You have to have a @ and a . symbol. '
+            if len(email) < 3 or len(email) > 20:
+                mail_error = mail_error + 'Email has to be between 3 and 20 characters. '
+
+        if pass_error != '' or mail_error != '' or user_error != '':
+            return render_template('index.html', username_error=user_error, email_error=mail_error, password_error=pass_error, username=name, email=email)
+        else:
+            return redirect('/welcome?username={0}'.format(name))
+    else:
+        return render_template('index.html')
+
+@app.route("/welcome")
 def welcome():
-    name = request.form['username']
-    password = request.form['password']
-    password2 = request.form['password2']
-    email = request.form['email']
-
-    pass_error = ''
-    mail_error = ''
-    user_error = ''
-
-    if name == '':
-        user_error = user_error + 'Username cannot be blank. '
-    else:
-        if name.strip(' ') != name:
-            user_error = user_error + 'Spaces are not allowed in password. '
-        if len(name) < 3 or len(name) > 20:
-            user_error = user_error + 'Username has to be between 3 and 20 characters. '
-
-
-    if password == '':
-        pass_error = pass_error + 'Password Cannot be blank. '
-    else:
-        if password != password2:
-            pass_error = pass_error + 'Password does not Match. '
-        if password.strip(' ') != password:
-            pass_error = pass_error + 'Spaces are not allowed in password. '
-        if len(password) < 3 or len(password) > 20:
-            pass_error = pass_error + 'Password has to be between 3 and 20 characters. '
-
-    if email != '':
-        if email.strip(' ') != email:
-            mail_error = mail_error + 'You cannot have a space. '
-        if '@' not in email or '.' not in email:
-            mail_error = mail_error + 'You have to have a @ and a . symbol. '
-        if len(email) < 3 or len(email) > 20:
-            mail_error = mail_error + 'Email has to be between 3 and 20 characters. '
-
-    if pass_error != '' or mail_error != '' or user_error != '':
-        return render_template('index.html', username_error=user_error, email_error=mail_error, password_error=pass_error, username=name, email=email)
-
-        
-        
-
+    name = request.args.get('username')
     return render_template('welcome.html', user_name=name)
 
 app.run()
